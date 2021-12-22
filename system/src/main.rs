58bfecdf30eb395c1fa40a48e3057274227787c6
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-21 16:10:38 -0800

    add initial run logic

--- system/src/main.rs
+++ system/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use system::authorization_server::AuthorizationServer;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let authorization_server = AuthorizationServer::init().await;
+
+    authorization_server.run().await?;
+
+    Ok(())
 }
