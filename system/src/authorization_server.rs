019a6e386f680f6a91f21d1cb1436370cf483252
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-22 11:03:00 -0800

    add additional endpoints

--- system/src/authorization_server.rs
+++ system/src/authorization_server.rs
@@ -1,3 +1,6 @@
+use axum::handler::Handler;
+use axum::http::{StatusCode, Uri};
+use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 
@@ -9,10 +12,14 @@ impl AuthorizationServer {
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let client_registration = Router::new().route(
-            "/client_registration",
-            get(AuthorizationServer::client_registration),
-        );
+        let client_registration = Router::new()
+            .route("/authorization", get(AuthorizationServer::authorization))
+            .route(
+                "/client_registration",
+                get(AuthorizationServer::client_registration),
+            )
+            .route("/token", get(AuthorizationServer::token))
+            .fallback(AuthorizationServer::fallback.into_service());
 
         axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
             .serve(client_registration.into_make_service())
@@ -21,7 +28,19 @@ impl AuthorizationServer {
         Ok(())
     }
 
+    async fn authorization() -> &'static str {
+        "grant"
+    }
+
     async fn client_registration() -> &'static str {
         "hello from client registration!"
+    }
+
+    async fn fallback(uri: Uri) -> impl IntoResponse {
+        (StatusCode::NOT_FOUND, format!("No route for {}", uri))
+    }
+
+    async fn token() -> &'static str {
+        "token"
     }
 }
