bfd2aecbe0b64903e6c631da2c589190ef7ebfc7
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-07-19 16:52:33 -0700

    add initial tests

--- system/src/authorization_server/token.rs
+++ system/src/authorization_server/token.rs
@@ -45,3 +45,85 @@ impl AuthorizationServer {
         Ok(response)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::routing::{get, post};
+    use axum::Router;
+    use axum::Server;
+    use hyper::{Body, Method};
+    use std::net::SocketAddr;
+    use std::str::FromStr;
+
+    #[tokio::test]
+    async fn token_post() -> Result<(), Box<dyn std::error::Error>> {
+        let test_socket_address = SocketAddr::from_str("127.0.0.1:6749")?;
+        let test_endpoint = Router::new().route("/token", post(AuthorizationServer::token));
+
+        let test_server =
+            Server::bind(&test_socket_address).serve(test_endpoint.into_make_service());
+
+        tokio::spawn(async move {
+            test_server.await.unwrap();
+        });
+
+        let test_uri = http::uri::Builder::new()
+            .scheme("http")
+            .authority("127.0.0.1:6749")
+            .path_and_query("/token?grant_type=authorization_code&code=some_authorization_code")
+            .build()
+            .unwrap();
+        let test_request = http::request::Builder::new()
+            .uri(test_uri)
+            .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+            .method(Method::POST)
+            .body(Body::empty())
+            .unwrap();
+
+        let test_client = hyper::client::Client::new();
+        let test_response = test_client.request(test_request).await;
+
+        assert!(test_response.is_ok());
+        assert_eq!(test_response.as_ref().unwrap().status(), StatusCode::OK);
+        assert!(test_response
+            .as_ref()
+            .unwrap()
+            .headers()
+            .contains_key(CONTENT_TYPE));
+        assert_eq!(
+            test_response
+                .as_ref()
+                .unwrap()
+                .headers()
+                .get(CONTENT_TYPE)
+                .unwrap(),
+            "application/json",
+        );
+        // assert!(test_response
+        //     .as_ref()
+        //     .unwrap()
+        //     .headers()
+        //     .contains_key(LOCATION));
+        // assert_eq!(
+        //     test_response
+        //         .as_ref()
+        //         .unwrap()
+        //         .headers()
+        //         .get(LOCATION)
+        //         .unwrap(),
+        //     "code=some_code",
+        // );
+        // assert!(hyper::body::to_bytes(test_response.unwrap().body_mut())
+        //     .await?
+        //     .is_empty());
+        let test_response_body = hyper::body::to_bytes(test_response.unwrap().body_mut()).await?;
+        let test_response_json: AccessTokenResponse = serde_json::from_slice(&test_response_body)?;
+
+        assert_eq!(test_response_json.access_token, "some_access_token");
+        assert_eq!(test_response_json.token_type, AccessTokenType::Bearer);
+        assert_eq!(test_response_json.expires_in, 3600);
+
+        Ok(())
+    }
+}
