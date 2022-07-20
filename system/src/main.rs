83a429c8893e25fcb928519824bce1f5378fdf92
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-07-20 16:34:00 -0700

    add initial client registration impl

--- system/src/main.rs
+++ system/src/main.rs
@@ -1,14 +1,30 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use system::authorization_server::AuthorizationServer;
+use system::client_registration::ClientRegistration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let authorization_server_socket_address =
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 6749);
+    let ip_address = Ipv4Addr::new(127, 0, 0, 1);
+    let authorization_server_socket_address = SocketAddr::new(IpAddr::V4(ip_address), 6749);
     let authorization_server = AuthorizationServer::init(authorization_server_socket_address).await;
 
-    authorization_server.run().await?;
+    let authorization_server_handle = tokio::spawn(async move {
+        if let Err(error) = authorization_server.run().await {
+            println!("authorization server -> {:?}", error);
+        }
+    });
+
+    let client_registration_socket_address = SocketAddr::new(IpAddr::V4(ip_address), 7591);
+    let client_registration = ClientRegistration::init(client_registration_socket_address).await;
+
+    let client_registration_handle = tokio::spawn(async move {
+        if let Err(error) = client_registration.run().await {
+            println!("client registration -> {:?}", error);
+        }
+    });
+
+    tokio::try_join!(authorization_server_handle, client_registration_handle)?;
 
     Ok(())
 }
