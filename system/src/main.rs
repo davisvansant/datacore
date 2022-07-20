284ce4211ce5066028e0fa7568f46d6cecd5e4a6
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-07-20 15:32:24 -0700

    adjust authorization server port

--- system/src/main.rs
+++ system/src/main.rs
@@ -4,8 +4,9 @@ use system::authorization_server::AuthorizationServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let socket_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-    let authorization_server = AuthorizationServer::init(socket_address).await;
+    let authorization_server_socket_address =
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 6749);
+    let authorization_server = AuthorizationServer::init(authorization_server_socket_address).await;
 
     authorization_server.run().await?;
 
