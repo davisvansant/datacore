b3efeb9710b63a4d5d8926f5e92597bbcec0b0a6
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-07-25 19:22:02 -0700

    add initial ClientRegistrationError/into_response impl

--- system/src/client_registration/register/error.rs
+++ system/src/client_registration/register/error.rs
@@ -1,4 +1,29 @@
+use axum::body::Body;
+use axum::http::header::{CACHE_CONTROL, CONTENT_TYPE, PRAGMA};
+use axum::response::{IntoResponse, Response};
 use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ClientRegistrationError {
+    pub error: ClientRegistrationErrorCode,
+    pub error_description: String,
+}
+
+impl IntoResponse for ClientRegistrationError {
+    fn into_response(self) -> Response {
+        let json = serde_json::to_vec(&self).expect("client registration error json");
+
+        let response = Response::builder()
+            .header(CONTENT_TYPE, "application/json")
+            .header(CACHE_CONTROL, "no-store")
+            .header(PRAGMA, "no-cache")
+            .status(400)
+            .body(Body::from(json))
+            .expect("client registration error response");
+
+        response.into_response()
+    }
+}
 
 #[derive(Serialize)]
 #[serde(rename_all = "snake_case")]
