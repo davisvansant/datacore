ef03fb629147c3faf0859c616f23ec73c4616f4c
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-08-06 14:38:01 -0700

    adjust field/variable names

--- system/src/state.rs
+++ system/src/state.rs
@@ -5,21 +5,21 @@ pub use channel::Request;
 mod channel;
 
 pub struct State {
-    request: Receiver<Request>,
+    receiver: Receiver<Request>,
 }
 
 impl State {
     pub async fn init() -> (State, Sender<Request>) {
-        let (send_request, request) = channel(64);
+        let (sender, receiver) = channel(64);
 
-        (State { request }, send_request)
+        (State { receiver }, sender)
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        while let Some(request) = self.request.recv().await {
+        while let Some(request) = self.receiver.recv().await {
             match request {
                 Request::Ok(()) => println!("received a request!"),
-                Request::Shutdown => self.request.close(),
+                Request::Shutdown => self.receiver.close(),
             }
         }
 
