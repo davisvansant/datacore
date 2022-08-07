baf92e870c83ecdbfcf01d98da63d6c77ccaf079
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-08-06 18:40:33 -0700

    add initial pub type, send request impl

--- system/src/state/channel.rs
+++ system/src/state/channel.rs
@@ -1,4 +1,27 @@
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+
+pub type ReceiveRequest = Receiver<Request>;
+
+#[derive(Debug)]
 pub enum Request {
     Ok(()),
     Shutdown,
+}
+
+pub struct SendRequest {
+    channel: Sender<Request>,
+}
+
+impl SendRequest {
+    pub async fn init() -> (SendRequest, ReceiveRequest) {
+        let (sender, receiver) = channel(64);
+
+        (SendRequest { channel: sender }, receiver)
+    }
+
+    pub async fn ok(&self) -> Result<(), Box<dyn std::error::Error>> {
+        self.channel.send(Request::Ok(())).await?;
+
+        Ok(())
+    }
 }
