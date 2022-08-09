c6b872b4535539ea396669a342e74708c151a4f5
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-08-09 13:50:21 -0700

    add initial access tokens channel

---
+++ system/src/state/access_tokens/channel.rs
@@ -0,0 +1,20 @@
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+
+pub type ReceiveRequest = Receiver<Request>;
+
+#[derive(Debug)]
+pub enum Request {
+    Shutdown,
+}
+
+pub struct AccessTokensRequest {
+    channel: Sender<Request>,
+}
+
+impl AccessTokensRequest {
+    pub async fn init() -> (AccessTokensRequest, ReceiveRequest) {
+        let (sender, receiver) = channel(64);
+
+        (AccessTokensRequest { channel: sender }, receiver)
+    }
+}
