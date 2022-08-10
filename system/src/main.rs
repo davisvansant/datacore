bd58d631997debb32d97c85fe32a1d4530302fe9
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-08-09 17:18:43 -0700

    cleanup/adjust impl order, using request impls

--- system/src/main.rs
+++ system/src/main.rs
@@ -8,9 +8,38 @@ use system::state::client_registry::ClientRegistry;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut client_registry, client_registry_request) = ClientRegistry::init().await;
+
+    let client_registry_handle = tokio::spawn(async move {
+        if let Err(error) = client_registry.run().await {
+            println!("client registry -> {:?}", error);
+        }
+    });
+
+    let (mut authorization_codes, authorization_codes_request) = AuthorizationCodes::init().await;
+
+    let authorization_codes_handle = tokio::spawn(async move {
+        if let Err(error) = authorization_codes.run().await {
+            println!("authorization codes -> {:?}", error);
+        }
+    });
+
+    let (mut access_tokens, access_tokens_request) = AccessTokens::init().await;
+
+    let access_tokens_handle = tokio::spawn(async move {
+        if let Err(error) = access_tokens.run().await {
+            println!("access tokens -> {:?}", error);
+        }
+    });
+
     let ip_address = Ipv4Addr::new(127, 0, 0, 1);
     let authorization_server_socket_address = SocketAddr::new(IpAddr::V4(ip_address), 6749);
-    let authorization_server = AuthorizationServer::init(authorization_server_socket_address).await;
+    let authorization_server = AuthorizationServer::init(
+        authorization_server_socket_address,
+        authorization_codes_request,
+        access_tokens_request,
+    )
+    .await;
 
     let authorization_server_handle = tokio::spawn(async move {
         if let Err(error) = authorization_server.run().await {
@@ -19,35 +48,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let client_registration_socket_address = SocketAddr::new(IpAddr::V4(ip_address), 7591);
-    let client_registration = ClientRegistration::init(client_registration_socket_address).await;
+    let client_registration =
+        ClientRegistration::init(client_registration_socket_address, client_registry_request).await;
 
     let client_registration_handle = tokio::spawn(async move {
         if let Err(error) = client_registration.run().await {
             println!("client registration -> {:?}", error);
-        }
-    });
-
-    let (mut client_registry, _client_registry_request) = ClientRegistry::init().await;
-
-    let client_registry_handle = tokio::spawn(async move {
-        if let Err(error) = client_registry.run().await {
-            println!("client registry -> {:?}", error);
-        }
-    });
-
-    let (mut authorization_codes, _authorization_codes_request) = AuthorizationCodes::init().await;
-
-    let authorization_codes_handle = tokio::spawn(async move {
-        if let Err(error) = authorization_codes.run().await {
-            println!("authorization codes -> {:?}", error);
-        }
-    });
-
-    let (mut access_tokens, _access_tokens_request) = AccessTokens::init().await;
-
-    let access_tokens_handle = tokio::spawn(async move {
-        if let Err(error) = access_tokens.run().await {
-            println!("access tokens -> {:?}", error);
         }
     });
 
