a9763a5e22a6bac812826e52c7af6b2e74efd357
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-09-22 13:03:23 -0700

    adjust struct name, add initial impl

--- authentication/src/authenticator/data.rs
+++ authentication/src/authenticator/data.rs
@@ -1,7 +1,25 @@
-pub struct Data {
+pub struct AuthenticatorData {
     pub rpidhash: String,
     pub flags: String,
     pub signcount: String,
     pub attestedcredentialdata: String,
     pub extensions: String,
+}
+
+impl AuthenticatorData {
+    pub async fn generate() -> AuthenticatorData {
+        let rpidhash = String::from("some_rpid_hash");
+        let flags = String::from("some_flags");
+        let signcount = String::from("some_sign_count");
+        let attestedcredentialdata = String::from("some_attested_credential_data");
+        let extensions = String::from("some_exentions");
+
+        AuthenticatorData {
+            rpidhash,
+            flags,
+            signcount,
+            attestedcredentialdata,
+            extensions,
+        }
+    }
 }
