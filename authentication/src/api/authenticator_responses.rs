a49c1b3dada16bb99c183b168c021cb1218e276a
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-09-23 17:14:50 -0700

    adjust to use enum, adding pub and initial impl

--- authentication/src/api/authenticator_responses.rs
+++ authentication/src/api/authenticator_responses.rs
@@ -1,10 +1,24 @@
-pub struct AuthenticatorResponse {
-    client_data_json: Vec<u8>,
+pub enum AuthenticatorResponse {
+    AuthenticatorAttestationResponse(AuthenticatorAttestationResponse),
+    AuthenticatorAssertionResponse(AuthenticatorAssertionResponse),
 }
 
+#[derive(Clone)]
 pub struct AuthenticatorAttestationResponse {
-    client_data_json: Vec<u8>,
-    attestation_object: Vec<u8>,
+    pub client_data_json: Vec<u8>,
+    pub attestation_object: Vec<u8>,
+}
+
+impl AuthenticatorAttestationResponse {
+    pub async fn generate() -> AuthenticatorAttestationResponse {
+        let client_data_json = Vec::with_capacity(0);
+        let attestation_object = Vec::with_capacity(0);
+
+        AuthenticatorAttestationResponse {
+            client_data_json,
+            attestation_object,
+        }
+    }
 }
 
 pub struct AuthenticatorAssertionResponse {
