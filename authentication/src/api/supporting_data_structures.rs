43b2e1fd0f8858f59155fe47da5002db8076642b
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-09-23 17:18:29 -0700

    add initial impl, adjust struct field visibility, add derive statements

--- authentication/src/api/supporting_data_structures.rs
+++ authentication/src/api/supporting_data_structures.rs
@@ -1,16 +1,45 @@
 pub struct CollectedClientData {
-    signature_confusion_attack_type: String,
-    challenge: String,
-    origin: String,
-    cross_origin: bool,
-    token_binding: Option<TokenBinding>,
+    pub r#type: String,
+    pub challenge: Vec<u8>,
+    pub origin: String,
+    pub cross_origin: bool,
+    pub token_binding: Option<TokenBinding>,
 }
 
+impl CollectedClientData {
+    pub async fn generate() -> CollectedClientData {
+        let r#type = String::from("some_type");
+        let challenge = Vec::with_capacity(0);
+        let origin = String::from("some_origin");
+        let cross_origin = false;
+        let token_binding = Some(TokenBinding::generate().await);
+
+        CollectedClientData {
+            r#type,
+            challenge,
+            origin,
+            cross_origin,
+            token_binding,
+        }
+    }
+}
+
+#[derive(PartialEq)]
 pub struct TokenBinding {
     status: TokenBindingStatus,
     id: String,
 }
 
+impl TokenBinding {
+    pub async fn generate() -> TokenBinding {
+        let status = TokenBindingStatus::Present;
+        let id = String::from("some_id");
+
+        TokenBinding { status, id }
+    }
+}
+
+#[derive(PartialEq)]
 pub enum TokenBindingStatus {
     Present,
     Supported,
