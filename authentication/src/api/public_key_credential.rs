9a36a0bb8b7acc104df146a25ea0336dfc9c2ae1
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-09-23 17:17:23 -0700

    adjust struct field visibility, add initial impl

--- authentication/src/api/public_key_credential.rs
+++ authentication/src/api/public_key_credential.rs
@@ -1,12 +1,32 @@
 use crate::api::assertion_generation_options::PublicKeyCredentialRequestOptions;
-use crate::api::authenticator_responses::AuthenticatorResponse;
+use crate::api::authenticator_responses::{
+    AuthenticatorAttestationResponse, AuthenticatorResponse,
+};
 use crate::api::credential_creation_options::PublicKeyCredentialCreationOptions;
 
 pub struct PublicKeyCredential {
     id: String,
     raw_id: Vec<u8>,
-    response: AuthenticatorResponse,
+    pub response: AuthenticatorResponse,
     r#type: String,
+}
+
+impl PublicKeyCredential {
+    pub async fn generate() -> PublicKeyCredential {
+        let id = String::from("some_id");
+        let raw_id = Vec::with_capacity(0);
+        let response = AuthenticatorResponse::AuthenticatorAttestationResponse(
+            AuthenticatorAttestationResponse::generate().await,
+        );
+        let r#type = String::from("some_type");
+
+        PublicKeyCredential {
+            id,
+            raw_id,
+            response,
+            r#type,
+        }
+    }
 }
 
 pub struct CredentialCreationOptions {
