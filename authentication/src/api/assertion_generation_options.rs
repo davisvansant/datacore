c8af9fc256d7a8863e21472ba801977619aa532c
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-09-27 17:32:00 -0700

    add initial impl, adjust struct field visibility

--- authentication/src/api/assertion_generation_options.rs
+++ authentication/src/api/assertion_generation_options.rs
@@ -2,10 +2,30 @@ use crate::api::extensions_inputs_and_outputs::AuthenticationExtensionsClientInp
 use crate::api::supporting_data_structures::PublicKeyCredentialDescriptor;
 
 pub struct PublicKeyCredentialRequestOptions {
-    challenge: Vec<u8>,
-    timeout: u32,
-    rp_id: String,
-    allow_credentials: Vec<PublicKeyCredentialDescriptor>,
-    user_verification: String,
-    extensions: Option<AuthenticationExtensionsClientInputs>,
+    pub challenge: Vec<u8>,
+    pub timeout: u32,
+    pub rp_id: String,
+    pub allow_credentials: Vec<PublicKeyCredentialDescriptor>,
+    pub user_verification: String,
+    pub extensions: Option<AuthenticationExtensionsClientInputs>,
+}
+
+impl PublicKeyCredentialRequestOptions {
+    pub async fn generate() -> PublicKeyCredentialRequestOptions {
+        let challenge = Vec::with_capacity(0);
+        let timeout = 0;
+        let rp_id = String::from("some_rp_id");
+        let allow_credentials = Vec::with_capacity(0);
+        let user_verification = String::from("preferred");
+        let extensions = None;
+
+        PublicKeyCredentialRequestOptions {
+            challenge,
+            timeout,
+            rp_id,
+            allow_credentials,
+            user_verification,
+            extensions,
+        }
+    }
 }
