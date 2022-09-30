c2d74137813af4561f37a6a299c7d69c93a45279
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-09-29 17:32:16 -0700

    use new generate impl

--- authentication/src/relying_party/operation/register.rs
+++ authentication/src/relying_party/operation/register.rs
@@ -1,7 +1,9 @@
 use crate::api::authenticator_responses::{
     AuthenticatorAttestationResponse, AuthenticatorResponse,
 };
-use crate::api::credential_creation_options::PublicKeyCredentialCreationOptions;
+use crate::api::credential_creation_options::{
+    PublicKeyCredentialCreationOptions, PublicKeyCredentialUserEntity,
+};
 use crate::api::extensions_inputs_and_outputs::AuthenticationExtensionsClientOutputs;
 use crate::api::public_key_credential::PublicKeyCredential;
 use crate::api::supporting_data_structures::{CollectedClientData, TokenBinding};
@@ -19,8 +21,11 @@ impl Register {
     pub async fn public_key_credential_creation_options(
         &self,
     ) -> Result<PublicKeyCredentialCreationOptions, AuthenticationError> {
+        let user = String::from("some_user");
+        let display_name = String::from("some_display_name");
+        let user_entity = PublicKeyCredentialUserEntity::generate(user, display_name).await;
         let public_key_credential_creation_options =
-            PublicKeyCredentialCreationOptions::generate().await;
+            PublicKeyCredentialCreationOptions::generate(user_entity).await;
 
         Ok(public_key_credential_creation_options)
     }
