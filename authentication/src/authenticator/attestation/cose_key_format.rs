cb517edf8f757c51cd5ab291521b9d858d656982
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-10-07 11:44:52 -0700

    add initial derive statements, further impl adjustments/additions

--- authentication/src/authenticator/attestation/cose_key_format.rs
+++ authentication/src/authenticator/attestation/cose_key_format.rs
@@ -1,27 +1,67 @@
-pub struct COSEKeyCommonParameters {
-    kty: KeyType,
-    kid: String,
-    alg: COSEAlgorithms,
-    key_ops: Vec<KeyOperations>,
-    base_IV: String,
+use serde::{Deserialize, Serialize};
+
+use crate::api::supporting_data_structures::COSEAlgorithmIdentifier;
+
+#[derive(Deserialize, Clone, Serialize)]
+pub struct COSEKey {
+    pub kty: KeyType,
+    pub kid: String,
+    pub alg: COSEAlgorithmIdentifier,
+    pub key_ops: Vec<KeyOperation>,
+    pub base_iv: Option<String>,
 }
 
+impl COSEKey {
+    pub async fn generate(algorithm: COSEAlgorithm) -> COSEKey {
+        let mut key_ops = Vec::with_capacity(10);
+
+        match algorithm {
+            COSEAlgorithm::EdDSA => COSEKey {
+                kty: KeyType::OKP,
+                kid: String::from("some_key_id"),
+                alg: COSEAlgorithm::EdDSA.identifier().await,
+                key_ops: {
+                    key_ops.push(KeyOperation::Sign);
+                    key_ops.push(KeyOperation::Verify);
+
+                    key_ops
+                },
+                base_iv: None,
+            },
+            COSEAlgorithm::ES256 => unimplemented!(),
+        }
+    }
+}
+
+#[derive(Deserialize, Clone, Serialize)]
 pub enum KeyType {
     OKP = 1,
     EC2 = 2,
     Symmetric = 4,
 }
 
-pub enum COSEAlgorithms {
-    EdDSA = -8,
-    ES256 = -7,
+#[derive(Deserialize, Clone, PartialEq, Serialize)]
+pub enum COSEAlgorithm {
+    EdDSA,
+    ES256,
 }
 
+impl COSEAlgorithm {
+    pub async fn identifier(&self) -> COSEAlgorithmIdentifier {
+        match self {
+            COSEAlgorithm::EdDSA => -8,
+            COSEAlgorithm::ES256 => -7,
+        }
+    }
+}
+
+#[derive(Deserialize, Clone, Serialize)]
 pub enum EllipticCurve {
     Ed25519 = 6,
 }
 
-pub enum KeyOperations {
+#[derive(Deserialize, Clone, Serialize)]
+pub enum KeyOperation {
     Sign = 1,
     Verify = 2,
     Encrypt = 3,
@@ -34,6 +74,7 @@ pub enum KeyOperations {
     MACVerify = 10,
 }
 
+#[derive(Deserialize, Clone, Serialize)]
 pub struct OctetKeyPair {
     crv: EllipticCurve,
     x: [u8; 32],
