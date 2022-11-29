d6adca941941717bdb1e4c3ebf4e8e085bb26f79
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-11-28 16:19:35 -0800

    adjust to use cose key type

--- authentication/src/authenticator/public_key_credential_source.rs
+++ authentication/src/authenticator/public_key_credential_source.rs
@@ -1,10 +1,12 @@
 use crate::api::supporting_data_structures::PublicKeyCredentialType;
+use crate::authenticator::attestation::{COSEAlgorithm, COSEKey};
 
 #[derive(Clone, Debug)]
 pub struct PublicKeyCredentialSource {
     pub r#type: PublicKeyCredentialType,
     pub id: [u8; 16],
-    pub private_key: Vec<u8>,
+    // pub private_key: Vec<u8>,
+    pub private_key: COSEKey,
     pub rpid: String,
     pub user_handle: [u8; 16],
     pub other_ui: String,
@@ -14,7 +16,8 @@ impl PublicKeyCredentialSource {
     pub async fn generate() -> PublicKeyCredentialSource {
         let r#type = PublicKeyCredentialType::PublicKey;
         let id = [0; 16];
-        let private_key = Vec::with_capacity(0);
+        // let private_key = Vec::with_capacity(0);
+        let private_key = COSEKey::generate(COSEAlgorithm::EdDSA).await.1;
         let rpid = String::from("some_rpid");
         let user_handle = [0; 16];
         let other_ui = String::from("some_other_ui");
