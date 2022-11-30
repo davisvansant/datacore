79e6970dc4cfe44ffebe9a0be1600984c6987b1b
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-11-30 11:40:37 -0800

    adjust generate impl, removing to byte array, update tests

--- authentication/src/authenticator/data.rs
+++ authentication/src/authenticator/data.rs
@@ -13,25 +13,77 @@ pub type SignCount = [u8; 4];
 pub struct AuthenticatorData {
     pub rp_id_hash: RpIdHash,
     pub flags: u8,
-    pub signcount: SignCount,
+    // pub signcount: SignCount,
+    pub sign_count: SignCount,
     pub attested_credential_data: Option<Vec<u8>>,
-    pub extensions: Option<String>,
+    // pub extensions: Option<String>,
+    pub extensions: Option<Vec<u8>>,
 }
 
 impl AuthenticatorData {
-    pub async fn generate(rp_id: &str, attested_credential_data: Vec<u8>) -> AuthenticatorData {
+    pub async fn generate(
+        rp_id: &str,
+        user_present: bool,
+        user_verified: bool,
+        sign_count: SignCount,
+        attested_credential_data: Option<Vec<u8>>,
+        extensions: Option<Vec<u8>>,
+    ) -> Vec<u8> {
         let rp_id_hash = generate_hash(rp_id.as_bytes()).await;
         let flags = 0b0000_0000;
-        let signcount = 0_u32.to_be_bytes();
-        // let extensions = String::from("some_extensions");
 
-        AuthenticatorData {
+        let mut authenticator_data = AuthenticatorData {
             rp_id_hash,
             flags,
-            signcount,
-            attested_credential_data: Some(attested_credential_data),
-            extensions: None,
+            sign_count,
+            attested_credential_data,
+            extensions,
+        };
+
+        if user_present {
+            authenticator_data.set_user_present().await;
         }
+
+        if user_verified {
+            authenticator_data.set_user_verifed().await;
+        }
+
+        if authenticator_data.attested_credential_data.is_some() {
+            authenticator_data
+                .set_attested_credential_data_included()
+                .await;
+        }
+
+        if authenticator_data.extensions.is_some() {
+            authenticator_data.set_extension_data_included().await;
+        }
+
+        let mut authenticator_data_byte_array: Vec<u8> = Vec::with_capacity(1000);
+
+        for element in &authenticator_data.rp_id_hash {
+            authenticator_data_byte_array.push(*element);
+        }
+
+        authenticator_data_byte_array.push(authenticator_data.flags);
+
+        for element in authenticator_data.sign_count {
+            authenticator_data_byte_array.push(element);
+        }
+
+        if let Some(attested_credential_data) = &authenticator_data.attested_credential_data {
+            for element in attested_credential_data {
+                authenticator_data_byte_array.push(*element);
+            }
+        }
+
+        if let Some(extensions) = &authenticator_data.extensions {
+            for element in extensions {
+                authenticator_data_byte_array.push(*element);
+            }
+        }
+
+        authenticator_data_byte_array.shrink_to_fit();
+        authenticator_data_byte_array
     }
 
     pub async fn set_user_present(&mut self) {
@@ -74,42 +126,19 @@ impl AuthenticatorData {
         (1 << ED & self.flags) > 0
     }
 
-    pub async fn to_byte_array(&self) -> Vec<u8> {
-        let mut authenticator_data: Vec<u8> = Vec::with_capacity(1000);
-
-        for element in &self.rp_id_hash {
-            authenticator_data.push(*element);
-        }
-
-        authenticator_data.push(self.flags);
-
-        for element in self.signcount {
-            authenticator_data.push(element);
-        }
-
-        if let Some(attested_credential_data) = &self.attested_credential_data {
-            for element in attested_credential_data {
-                authenticator_data.push(*element);
-            }
-        }
-
-        authenticator_data.shrink_to_fit();
-        authenticator_data
-    }
-
     pub async fn from_byte_array(data: &[u8]) -> AuthenticatorData {
         if data.len() == 37 {
             let (rp_id_hash, remaining) = data.split_at(32);
             let (flags, sign_count_data) = remaining.split_at(1);
 
-            let mut signcount: [u8; 4] = [0; 4];
+            let mut sign_count: [u8; 4] = [0; 4];
 
-            signcount.copy_from_slice(sign_count_data);
+            sign_count.copy_from_slice(sign_count_data);
 
             AuthenticatorData {
                 rp_id_hash: rp_id_hash.to_vec(),
                 flags: flags[0],
-                signcount,
+                sign_count,
                 attested_credential_data: None,
                 extensions: None,
             }
@@ -118,14 +147,14 @@ impl AuthenticatorData {
             let (flags, remaining) = remaining.split_at(1);
             let (sign_count_data, attested_credential_data) = remaining.split_at(4);
 
-            let mut signcount: [u8; 4] = [0; 4];
+            let mut sign_count: [u8; 4] = [0; 4];
 
-            signcount.copy_from_slice(sign_count_data);
+            sign_count.copy_from_slice(sign_count_data);
 
             AuthenticatorData {
                 rp_id_hash: rp_id_hash.to_vec(),
                 flags: flags[0],
-                signcount,
+                sign_count,
                 attested_credential_data: Some(attested_credential_data.to_vec()),
                 extensions: None,
             }
@@ -139,41 +168,41 @@ mod tests {
     use crate::authenticator::attestation::{AttestedCredentialData, COSEAlgorithm, COSEKey};
 
     #[tokio::test]
-    async fn init() -> Result<(), Box<dyn std::error::Error>> {
+    async fn generate() -> Result<(), Box<dyn std::error::Error>> {
         let test_rp_id = "test_rp_id";
-        let test_credential_id = [0u8; 16];
-        let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
-        let test_attested_credential_data =
-            AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
-        let test_rp_hash = generate_hash(test_rp_id.as_bytes()).await;
+        let test_user_present = true;
+        let test_user_verified = true;
+        let test_sign_count = [0u8; 4];
+        let test_authenticator_data = AuthenticatorData::generate(
+            test_rp_id,
+            test_user_present,
+            test_user_verified,
+            test_sign_count,
+            None,
+            None,
+        )
+        .await;
 
-        assert_eq!(test_authenticator_data.rp_id_hash, test_rp_hash);
-        assert_eq!(std::mem::size_of_val(&test_authenticator_data.flags), 1);
-        assert_eq!(test_authenticator_data.signcount, [0; 4]);
+        for element in &test_authenticator_data {
+            assert_eq!(std::mem::size_of_val(element), 1);
+        }
 
-        test_authenticator_data.set_user_present().await;
-        test_authenticator_data.set_user_verifed().await;
-        test_authenticator_data
-            .set_attested_credential_data_included()
-            .await;
-        test_authenticator_data.set_extension_data_included().await;
-
-        assert_eq!(std::mem::size_of_val(&test_authenticator_data.flags), 1);
+        assert_eq!(test_authenticator_data.len(), 37);
+        assert_eq!(test_authenticator_data.capacity(), 37);
+        assert_eq!(std::mem::size_of_val(&*test_authenticator_data), 37);
 
         Ok(())
     }
 
     #[tokio::test]
     async fn flags_user_present() -> Result<(), Box<dyn std::error::Error>> {
-        let test_rp_id = "test_rp_id";
-        let test_credential_id = [0u8; 16];
-        let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
-        let test_attested_credential_data =
-            AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
+        let mut test_authenticator_data = AuthenticatorData {
+            rp_id_hash: generate_hash(b"test_rp_id").await,
+            flags: 0b0000_0000,
+            sign_count: [0u8; 4],
+            attested_credential_data: None,
+            extensions: None,
+        };
 
         assert_eq!(UP, 0);
         assert_eq!(std::mem::size_of_val(&test_authenticator_data.flags), 1);
@@ -194,13 +223,13 @@ mod tests {
 
     #[tokio::test]
     async fn flags_user_verified() -> Result<(), Box<dyn std::error::Error>> {
-        let test_rp_id = "test_rp_id";
-        let test_credential_id = [0u8; 16];
-        let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
-        let test_attested_credential_data =
-            AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
+        let mut test_authenticator_data = AuthenticatorData {
+            rp_id_hash: generate_hash(b"test_rp_id").await,
+            flags: 0b0000_0000,
+            sign_count: [0u8; 4],
+            attested_credential_data: None,
+            extensions: None,
+        };
 
         assert_eq!(UV, 2);
         assert_eq!(std::mem::size_of_val(&test_authenticator_data.flags), 1);
@@ -220,13 +249,13 @@ mod tests {
 
     #[tokio::test]
     async fn flags_attested_credential_data() -> Result<(), Box<dyn std::error::Error>> {
-        let test_rp_id = "test_rp_id";
-        let test_credential_id = [0u8; 16];
-        let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
-        let test_attested_credential_data =
-            AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
+        let mut test_authenticator_data = AuthenticatorData {
+            rp_id_hash: generate_hash(b"test_rp_id").await,
+            flags: 0b0000_0000,
+            sign_count: [0u8; 4],
+            attested_credential_data: None,
+            extensions: None,
+        };
 
         assert_eq!(AT, 6);
         assert_eq!(std::mem::size_of_val(&test_authenticator_data.flags), 1);
@@ -252,13 +281,13 @@ mod tests {
 
     #[tokio::test]
     async fn flags_extension_data() -> Result<(), Box<dyn std::error::Error>> {
-        let test_rp_id = "test_rp_id";
-        let test_credential_id = [0u8; 16];
-        let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
-        let test_attested_credential_data =
-            AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
+        let mut test_authenticator_data = AuthenticatorData {
+            rp_id_hash: generate_hash(b"test_rp_id").await,
+            flags: 0b0000_0000,
+            sign_count: [0u8; 4],
+            attested_credential_data: None,
+            extensions: None,
+        };
 
         assert_eq!(ED, 7);
         assert_eq!(std::mem::size_of_val(&test_authenticator_data.flags), 1);
@@ -273,62 +302,26 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn to_byte_array() -> Result<(), Box<dyn std::error::Error>> {
-        let test_rp_id = "test_rp_id";
-        let test_credential_id = [0u8; 16];
-        let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
-        let test_attested_credential_data =
-            AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
-
-        let test_byte_array = test_authenticator_data.to_byte_array().await;
-
-        for element in &test_byte_array {
-            assert_eq!(std::mem::size_of_val(element), 1);
-        }
-
-        assert!(test_byte_array.len() >= 37);
-        assert!(test_byte_array.capacity() >= 37);
-        assert!(std::mem::size_of_val(&*test_byte_array) >= 37);
-
-        test_authenticator_data
-            .set_attested_credential_data_included()
-            .await;
-        test_authenticator_data.set_extension_data_included().await;
-        test_authenticator_data.signcount = 1000_u32.to_be_bytes();
-
-        let test_other_byte_array = test_authenticator_data.to_byte_array().await;
-
-        for element in &test_other_byte_array {
-            assert_eq!(std::mem::size_of_val(element), 1);
-        }
-
-        assert!(test_other_byte_array.len() >= 37);
-        assert!(test_other_byte_array.capacity() >= 37);
-        assert!(std::mem::size_of_val(&*test_other_byte_array) >= 37);
-
-        Ok(())
-    }
-
-    #[tokio::test]
     async fn from_byte_array() -> Result<(), Box<dyn std::error::Error>> {
-        let test_rp_id = "test_rp_id";
         let test_credential_id = [0u8; 16];
         let test_keypair = COSEKey::generate(COSEAlgorithm::EdDSA).await;
         let test_attested_credential_data =
             AttestedCredentialData::generate(test_credential_id, test_keypair.0).await?;
-        let mut test_authenticator_data =
-            AuthenticatorData::generate(test_rp_id, test_attested_credential_data).await;
-
-        test_authenticator_data
-            .set_attested_credential_data_included()
-            .await;
-        test_authenticator_data.set_extension_data_included().await;
-        test_authenticator_data.signcount = 1000_u32.to_be_bytes();
-
-        let test_byte_array = test_authenticator_data.to_byte_array().await;
-        let test_from_byte_array = AuthenticatorData::from_byte_array(&test_byte_array).await;
+        let test_rp_id = "test_rp_id";
+        let test_user_present = true;
+        let test_user_verified = true;
+        let test_sign_count = 1000_u32.to_be_bytes();
+        let test_authenticator_data = AuthenticatorData::generate(
+            test_rp_id,
+            test_user_present,
+            test_user_verified,
+            test_sign_count,
+            Some(test_attested_credential_data),
+            None,
+        )
+        .await;
+        let test_from_byte_array =
+            AuthenticatorData::from_byte_array(&test_authenticator_data).await;
 
         assert_eq!(
             test_from_byte_array.rp_id_hash,
@@ -339,8 +332,8 @@ mod tests {
                 .includes_attested_credential_data()
                 .await
         );
-        assert!(test_from_byte_array.includes_extension_data().await);
-        assert_eq!(test_from_byte_array.signcount, 1000_u32.to_be_bytes());
+        assert!(!test_from_byte_array.includes_extension_data().await);
+        assert_eq!(test_from_byte_array.sign_count, 1000_u32.to_be_bytes());
         assert!(test_from_byte_array.attested_credential_data.is_some());
         assert!(test_from_byte_array.extensions.is_none());
 
