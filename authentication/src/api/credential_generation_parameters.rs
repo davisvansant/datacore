4da6f8b727547e63314ff1166b3ee5c920960269
Davis Van Sant <davisvansant@users.noreply.github.com>
2023-01-05 10:35:44 -0800

    add initial tests

--- authentication/src/api/credential_generation_parameters.rs
+++ authentication/src/api/credential_generation_parameters.rs
@@ -7,3 +7,48 @@ pub struct PublicKeyCredentialParameters {
     pub r#type: PublicKeyCredentialType,
     pub alg: COSEAlgorithmIdentifier,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn to_json() -> Result<(), Box<dyn std::error::Error>> {
+        let test_parameters = PublicKeyCredentialParameters {
+            r#type: PublicKeyCredentialType::PublicKey,
+            alg: -8,
+        };
+
+        assert!(serde_json::to_string(&test_parameters).is_ok());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn from_json() -> Result<(), Box<dyn std::error::Error>> {
+        let test_json = r#"
+            {
+                "type": "public-key",
+                "alg": -8
+            }
+        "#
+        .as_bytes();
+
+        let test_parameters: PublicKeyCredentialParameters = serde_json::from_slice(test_json)?;
+
+        assert_eq!(test_parameters.r#type, PublicKeyCredentialType::PublicKey);
+        assert_eq!(test_parameters.alg, -8);
+
+        let test_json = r#"
+            {
+                "type": "publickey",
+                "alg": "-8"
+            }
+        "#
+        .as_bytes();
+
+        assert!(serde_json::from_slice::<PublicKeyCredentialParameters>(test_json).is_err());
+
+        Ok(())
+    }
+}
